fd7f94c53a7c5882b9058b8a2f663280ddcd4685
Bhargav <bhargavkadali39@gmail.com>
2021-10-20 20:28:48 +0530

    Update platform_info.py

--- platform_info.py
+++ platform_info.py
@@ -1,1 +1,9 @@
 
+import platform
+
+print("System:",platform.system())
+print("node:",platform.node())
+print("platform:",platform.platform())
+print("processor:",platform.processor())
+print("Python version:", platform.python_version())
+print("Python implementation:", platform.python_implementation())
